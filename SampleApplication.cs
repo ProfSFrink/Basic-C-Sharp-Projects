04fecbf0e9dff76a99cb74f1abb8a51c7f99025c
Steven Partlow <97795561+ProfSFrink@users.noreply.github.com>
2022-11-22 15:47:59 +0000

    Completed Compiler Assignment Part 1,2 and 3
    Compiler Assignment - Part 1:
    - Added "csc.exe" to PATH environment variable.
    Compiler Assignment - Part 2:
    - Created folder "Basic_C#_Programs"
    - Created file "SampleApplication.cs"
    - Added code to print "Hello, World" to the console
    Compiler Assignment - Part 3:
    - Utilised the C# compiler to create an executable called "SampleApplication.exe" and ran it successfully

---
+++ SampleApplication.cs
@@ -0,0 +1,14 @@
+// Advanced Software and Web Developer Diploma
+// Part IX: C# and .NET Framework - Part 1
+// by Pitman Training / The Tech Academy
+// AUTHOR: Steven Partlow
+
+using System; // Use the Basic class library
+
+class SampleApplication // Everything between the curly braces is part of the group of code called "SampleApplication"
+{
+    static void Main() // Execute everything between the curly braces
+    {
+        Console.WriteLine("Hello, World"); // Print the text "Hello, World" to the terminal window
+    } // End MAIN
+} // End CLASS
