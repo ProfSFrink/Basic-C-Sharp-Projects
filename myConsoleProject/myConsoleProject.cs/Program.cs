1c8ccc38dc5d6b2b3b036123811700c57c792ad5
Steven Partlow <97795561+ProfSFrink@users.noreply.github.com>
2022-11-24 10:46:02 +0000

    Visual Studio Assignment - Part 3: - Print text asking user their name to the console window. - Accept input from the user and store in variable 'name'. - Print the value of 'name' within a string to the console window. - Prevent the console window from closing before the application exits. The user must press ENTER to close the window. - Add comments to code.

--- myConsoleProject/myConsoleProject.cs/Program.cs
+++ myConsoleProject/myConsoleProject.cs/Program.cs
@@ -5,14 +5,20 @@
 // MODULE 3: Visual Studio Assignment
 // AUTHOR: Steven Partlow
 
-using System;
+using System; // Use the SYSTEM library
 
 namespace myConsoleProject.cs
 {
     class Program
     {
-        static void Main()
+        static void Main() // The MAIN application - The first thing executed
         {
-        }
-    }
-}
+            Console.WriteLine("What is your name?"); // Print this text to the console
+            string name = Console.ReadLine(); // Take the text inputted by the user and store within variable 'name'
+            Console.WriteLine("Hello, " + name + "!"); // Print this text alongside the value stored in 'name' to the console
+            Console.Read(); // Do not close the console window until the user presses ENTER
+        } // End MAIN
+
+    } // End CLASS
+
+} // End Program
