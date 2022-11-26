7e4706a94acc11103691d25d47b485be46a0801c
Steven Partlow <profsfrink@gmail.com>
2022-11-26 12:29:42 +0000

    Complete Challenge 4B-1
    Challenge 4B-1: Escape Sequence:
    - Print the challenge title.
    - Print a string on new line using '\n' then use '\' to print my name to the console in double-quotes ("").
    - Commented Code.

---
+++ Challenge_4B-1/Challenge_4B-1_Escape_Sequence/Program.cs
@@ -0,0 +1,21 @@
+﻿// Advanced Software and Web Developer Diploma
+// Part IX: C# and .NET Framework - Part 1
+// by Pitman Training / The Tech Academy
+
+// MODULE 4: CHALLENGE 4B-1: Escape Sequence / Characters
+// AUTHOR: Steven Partlow
+
+using System; // Use the System Library
+
+class Program
+{
+	static void Main() // The method Main is the first thing that is executed when the console applicatio is run
+					   // Void mean we are not refering to an object and void means the method will not return a value
+	{
+		Console.WriteLine("CHALLENGE: 4B-1: Make use of escape characters"); // Display this text to the console window
+		Console.WriteLine("\nMy name is \"Steven Partlow\""); // Use escape characters (\n) to create a new line then use (/) to display my name is double-quotes ("")
+		Console.Read();
+	} // End MAIN
+
+} // End CLASS / PROGRAM
+
