5238875d0a2adc7b7c128f9e39b7cbb14ae427ac
Steven Partlow <97795561+ProfSFrink@users.noreply.github.com>
2022-11-29 11:24:46 +0000

    Update Challege 7B-1 - Math Operations: - Goal 1: Add two numbers and display the output complete.

---
+++ Challenge_7B-1/Challenge_7B-1_Math/Program.cs
@@ -0,0 +1,36 @@
+﻿// Advanced Software and Web Developer Diploma
+// Part IX: C# and .NET Framework - Part 1
+// by Pitman Training / The Tech Academy
+
+// MODULE 8: Challenge 7B-1: Math Operations 
+// AUTHOR: Steven Partlow
+// DATE: 29/11/2022
+
+// GOALS FOR THIS CHALLENGE
+// 1) Add two numbers and display the output - DONE
+// 2) Subtract two numbers and display the output
+// 3) Multiply two numbers and display the output
+// 4) Divide two numbers and display the output
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Challenge_7B_1_Math
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // 1) Add two numbers and display the output
+            int num1 = 43; // Initialise the variable as an integer and assign it this value
+            int num2 = 41; // Initialise the variable as an integer and assign it this value
+            int answer = num1 + num2; // Initialise the variable as an integer and assign it the value of num1 plus num2 (84)
+            Console.WriteLine(num1 + " + " + num2 + " = " + answer); // Output the answer to the console window
+
+            Console.ReadLine(); // Pause the application so we can see the output
+        }
+    }
+}
