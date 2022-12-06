074b4b04702e6f0efa7d0d56a68ef61629a200a8
Steven Partlow <profsfrink@gmail.com>
2022-12-06 11:35:52 +0000

    Complete Challenge 10A-1: - ADD Accept user input as a string and convert it to uppercase - ADD Execute IF statement if user enters SWITCH - ADD Execute ELSE IF statement if user enters PC - ADD Execute ELSE IF statement if user enters PLAYSTATION 5 - ADD Execute ELSE statement if user does not enter a valid platform

--- Challenge10A-1/Challenge10A-1/Program.cs
+++ Challenge10A-1/Challenge10A-1/Program.cs
@@ -27,8 +27,38 @@ namespace Challenge10A_1
             Console.WriteLine("CHALLENGE 10A-1: IF / ELSE / ELSE IF Statements");
             Console.WriteLine("-----------------------------------------------\n");
 
-            Console.WriteLine("\nPress the ENTER key to exit the application"); // Output this text to the display
-            Console.ReadLine(); // Pause the application so we can see the output
+            Console.Write("Please enter the name of a games console you own: "); // Display this text to the console
+            string userPlatform = Console.ReadLine().ToUpper(); // Accept input from the user, assign it to string variable userPlatform and covert it to uppercase
+
+            if (userPlatform == "SWITCH") // If the user platform equals SWITCH execute this IF statement
+            {
+                Console.WriteLine("\nThe next game you shoud play is: ATARI 50: The Anniversary Collection"); // Print this text to the console
+
+                Console.WriteLine("\nPress the ENTER key to exit the application"); // Output this text to the display
+                Console.ReadLine(); // Pause the application so we can see the output // End IF
+            }
+            else if (userPlatform == "PC") // If the user platform equals PC execute this IF statement
+            {
+                Console.WriteLine("\nThe next game you should play is: Vampire Survivors"); // Output this text to the display
+
+                Console.WriteLine("\nPress the ENTER key to exit the application"); // Output this text to the display
+                Console.ReadLine(); // Pause the application so we can see the output // End IF
+            } // End ELSE IF
+            else if (userPlatform == "PLAYSTATION 5") // If the user platform equals PLAYSTATION 5 execute this IF statement
+            {
+                Console.WriteLine("\nThe next game you should play is: Star Ocean: The Divine Force"); // Output this text to the console
+
+                Console.WriteLine("\nPress the ENTER key to exit the application"); // Output this text to the display
+                Console.ReadLine(); // Pause the application so we can see the output // End IF
+            } // End ELSE IF
+            else // If the user input does not equal any of the above statements then execute this ELSE statememnt
+            {
+                Console.WriteLine("\nYou did not enter a valid platform choice, please rerun the application and try again!");
+
+                Console.WriteLine("\nPress the ENTER key to exit the application"); // Output this text to the display
+                Console.ReadLine(); // Pause the application so we can see the output // End IF
+            } // End ELSE
+
         } // End MAIN
 
     } // End CLASS
