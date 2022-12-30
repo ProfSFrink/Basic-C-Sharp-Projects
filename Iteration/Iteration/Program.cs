e3ce6cd0c3c6c233eefa7f4b3a92036f91658bcf
Steven Partlow <profsfrink@gmail.com>
2022-12-30 22:07:23 +0000

    COMPLETE Iteration Part 2:
    - ADD Iterate through an array and print all values
    - ADD Create a list of integers to represent test scores
    - ADD Utilise a foreach loop to iterate through the list and print any value greater than 85 to the console
    - ADD Create a list of strings representing names
    - ADD Utilise a foreach loop to iterate through the list and print any value that equals Jesse
    - ADD Iterate through the list of names and print each one to the console
    - ADD Create a list of integer representing test scores and a second empty list for passing scores
    - ADD Iterate through the list of test scores and add any that are greater than 85 to rhe second empty list and after this process count the number of elements within the second list and output that value to the console
    - UPDATE readme.md add link to iteration tutorial source code
    - Fully comment code

--- Iteration/Iteration/Program.cs
+++ Iteration/Iteration/Program.cs
@@ -20,6 +20,8 @@ class Program
         Console.WriteLine("Iteration Tutorial");
         Console.WriteLine("------------------");
 
+        // Iteration - Part 1 (29/12/22)
+
         int[] testScores = { 98, 99, 85, 70, 82, 34, 91, 90, 94 }; // Instansiate a new array consisting of nine integer values to represent test scores
 
         for (int i = 0; i < testScores.Length; i++) // Start a FOR loop, with the counter variable i which will count from zero to the lenghth of the testScores array
@@ -42,7 +44,57 @@ class Program
                 Console.WriteLine(names[j]); // Print the name to the console
             } // End IF
 
+     // Iteration Tutorial -Part 2(30 / 12 / 22)
+
+            Console.WriteLine(names[j]); // This will print the current element being checked within the array
+
         } // End FOR
+
+        //List<int> testScores = new List<int>(); // Create an instance of a list object consisting of integer data types called testScores
+        //testScores.Add(98); // Add this value to the list testScores
+        //testScores.Add(99); // Add this value to the list testScores
+        //testScores.Add(81); // Add this value to the list testScores
+        //testScores.Add(72); // Add this value to the list testScores
+        //testScores.Add(70); // Add this value to the list testScores
+
+        foreach (int score in testScores) // Iterate through the list testScores assigning the currently checked value to the variable score
+        {
+            if (score > 85) // If the current value of score is greater than 85
+            {
+                Console.WriteLine("Passing test score: " + score); // Concatenate this string and print to the console
+            } // End IF
+        } // End FOREACH
+
+       // List<string> names = new List<string>() { "Jesse", "Erik", "Adam", "Daniel" }; // Create a new instance of a list object consisting of string data types called names and add four values to the list
+
+        foreach (string name in names) // Iterate through the list names assigning the currently check value to the variable name
+        {
+            if (name == "Jesse")  // If the current value name is equal to Jesse
+            {
+                Console.WriteLine(name); // Print the current value of the variable name to the console
+            } // End IF
+        } // End FOREACH
+
+        foreach (string name in names) // Iterate through the list of names assigning the currently checked value to the variable name
+        {
+            Console.WriteLine(name); // Print the current value of the variable name to the console
+        } // End FOREACH
+
+        // List<int> testScores = new List<int>() { 98, 99, 12, 74, 23, 99 }; // Create a new instance of a list object consisting of integer data types called testScores and add five values to the list
+        List<int> passingScores = new List<int>(); // Create an empty instance of the list object which will utilise integer values
+
+        foreach (int score in testScores) // Iterate through the list of testScores assigning the currently checked value to the variable score
+        {
+            if (score > 85) // If the current value of score is greater than 85
+            {
+                passingScores.Add(score); // Add the current value of score to the list passingscores
+            } // End IF
+
+        } // End FOREACH
+  
+        Console.WriteLine(passingScores.Count); // Count the number of total elements within the list passingScores and output the value to the console
+
+        Console.ReadLine(); // Pause the application so we can view the output
 
     } // End MAIN
 
