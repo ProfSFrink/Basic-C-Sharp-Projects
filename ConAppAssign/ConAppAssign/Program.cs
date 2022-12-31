a5e73a6ecad1a19b2cbd8d411a8dad1834a81077
Steven Partlow <profsfrink@gmail.com>
2022-12-31 12:30:22 +0000

    COMPLETE Console App Assignment Part 4:
    - COMPLETE GOAL 4-1) A list of strings where each item in the list is unique
    - COMPLETE GOAL 4-2) Ask the user to input text to search for in the list
    - COMPLETE GOAL 4-3) A loop that iterates through the list and then displays the index of the list that contains matching text on the screen
    COMPLETE GOAL 4-4) Add code to the loop that tells a user if they put in text that isn’t in the list
    COMPLETE GOAL 4-5) Add code to the loop that stops it from executing once a match has been found
    - ADD Full code comments
    - UPDATE readme.md assignment description

--- ConAppAssign/ConAppAssign/Program.cs
+++ ConAppAssign/ConAppAssign/Program.cs
@@ -30,6 +30,15 @@
 // 3-3) A loop where the comparison that’s used to determine whether to continue iterating the loop is a “<=” operator. - DONE
 // 3-4) Save and execute your code. - DONE
 
+// PART 4
+// ------
+// 4-1) A list of strings where each item in the list is unique. - DONE
+// 4-2) Ask the user to input text to search for in the list. - DONE
+// 4-3) A loop that iterates through the list and then displays the index of the list that contains matching text on the screen. - DONE
+// 4-4) Add code to the loop that tells a user if they put in text that isn’t in the list. - DONE
+// 4-5) Add code to the loop that stops it from executing once a match has been found. - DONE
+// 4-6) Save and execute your code. - DONE
+
 using System;
 using System.Collections.Generic;
 
@@ -38,76 +47,128 @@ class Program
 {
     static void Main()
     {
-        // Output all the below text to the display
-        Console.WriteLine("PITMAN TRAINING / THE TECH ACADEMY");
-        Console.WriteLine("----------------------------------\n");
-        
-        // PART 1
-        Console.WriteLine("Console App Assignment - Part 1"); // Output this text to the console then start a new line
-        Console.WriteLine("-------------------------------\n");
+        //// Output all the below text to the display
+        //Console.WriteLine("PITMAN TRAINING / THE TECH ACADEMY");
+        //Console.WriteLine("----------------------------------\n");
 
-        // GOAL 1-1) A one-dimensional array of strings
-        string[] games = { "Moon Patrol", "10-Yard Fight", "Battle Chopper", "Lightning Swords", "R-Type", "In the Hunt" }; // Instanstiate an array of string called games and add these six values to the array
+        ///* ---------------------------------------------------------------- PART 1 ----------------------------------------------------------------------------------- */
 
-        // GOAL 1-2) Ask the user to input some text
-        Console.Write("Please enter some text into the application, thank you: "); // Output this text to the console
-        string userInput = Console.ReadLine(); // Store the text the user enters into the string variable userInput
+        //Console.WriteLine("Console App Assignment - Part 1"); // Output this text to the console then start a new line
+        //Console.WriteLine("-------------------------------\n");
 
-        // GOAL 1-3) A loop that iterates through each string in the array and adds the user's text input to the end of each string
+        //// GOAL 1-1) A one-dimensional array of strings
+        //string[] games = { "Moon Patrol", "10-Yard Fight", "Battle Chopper", "Lightning Swords", "R-Type", "In the Hunt" }; // Instanstiate an array of string called games and add these six values to the array
 
-        Console.WriteLine(); // Adds a blank line to the console window
-        for (int i = 0; i < 6; i++) // Start a FOR loop using counter variable i start at zero and keep iterating until i is six or greater
-        {
-            games[i] = games[i] + " " + userInput; // Concatenate the current value of the games array with the userinput and store this new string in the current index of the games array
+        //// GOAL 1-2) Ask the user to input some text
+        //Console.Write("Please enter some text into the application, thank you: "); // Output this text to the console
+        //string userInput = Console.ReadLine(); // Store the text the user enters into the string variable userInput
 
-            // GOAL 1-4) Then create a loop that prints off each string in the array on a separate line (DOING THIS AS PART OF THE SAME LOOP AS MORE EFFICENT THAN USING TWO SEPERATE FOR LOOPS)
+        //// GOAL 1-3) A loop that iterates through each string in the array and adds the user's text input to the end of each string
 
-            Console.WriteLine(games[i]); // Output the current value of games to the console
-        } // End FOR
+        //Console.WriteLine(); // Adds a blank line to the console window
+        //for (int i = 0; i < 6; i++) // Start a FOR loop using counter variable i start at zero and keep iterating until i is six or greater
+        //{
+        //    games[i] = games[i] + " " + userInput; // Concatenate the current value of the games array with the userinput and store this new string in the current index of the games array
 
-        // PART 2
-        Console.WriteLine("\nConsole App Assignment - Part 2"); // Output this text to the console then start a new line
-        Console.WriteLine("-------------------------------\n");
+        //    // GOAL 1-4) Then create a loop that prints off each string in the array on a separate line (DOING THIS AS PART OF THE SAME LOOP AS MORE EFFICENT THAN USING TWO SEPERATE FOR LOOPS)
 
-        //// GOAL 2 - 1) An infinite loop
+        //    Console.WriteLine(games[i]); // Output the current value of games to the console
+        //} // End FOR
+
+        ///* ---------------------------------------------------------------- PART 2 ----------------------------------------------------------------------------------- */
+
+        //Console.WriteLine("\nConsole App Assignment - Part 2"); // Output this text to the console then start a new line
+        //Console.WriteLine("-------------------------------\n");
+
+        ////// GOAL 2 - 1) An infinite loop
+        ////bool infinite = true; // Initilise a boolean variable and set it to true
+
+        ////while (infinite) // This is an infinite while loop that will not stop looping as infinite will always be set to true
+        ////{
+        ////    Console.WriteLine("This is an infinite loop that will never end!. You will have force the application to quit!."); // Output this text to the console
+        ////} // End While
+
+        //// GOAL 2-3) Fix the infinite loop so that it will execute properly
         //bool infinite = true; // Initilise a boolean variable and set it to true
 
-        //while (infinite) // This is an infinite while loop that will not stop looping as infinite will always be set to true
+        //while (infinite) // This loop will now execute once as it will change infinite to false after one loop, exiting the while loop
         //{
-        //    Console.WriteLine("This is an infinite loop that will never end!. You will have force the application to quit!."); // Output this text to the console
+        //    infinite = false; // Set the boolean variable infinite to false to exit the while loop
+        //    Console.WriteLine("The while loop will now exit correctly!\n"); // Output this text to the console
         //} // End While
 
-        // GOAL 2-3) Fix the infinite loop so that it will execute properly
-        bool infinite = true; // Initilise a boolean variable and set it to true
+        ///* ---------------------------------------------------------------- PART 3 ----------------------------------------------------------------------------------- */
 
-        while (infinite) // This loop will now execute once as it will change infinite to false after one loop, exiting the while loop
-        {
-            infinite = false; // Set the boolean variable infinite to false to exit the while loop
-            Console.WriteLine("The while loop will now exit correctly!\n"); // Output this text to the console
-        } // End While
+        //Console.WriteLine("Console App Assignment - Part 3"); // Output this text to the console then start a new line
+        //Console.WriteLine("-------------------------------\n");
 
-        // PART 3
-        Console.WriteLine("Console App Assignment - Part 3"); // Output this text to the console then start a new line
+        //// GOAL 3-1) A loop where the comparison that’s used to determine whether to continue iterating the loop is a “<” operator
+        //for(int c = 1; c < 11; c++) // This FOR loop will count from 1 to 10 using the integer c as a counter
+        //{
+        //    Console.WriteLine("The loop has run " + c + " times."); // Concatenate c into this string and out it to the console.
+        //} // End FOR
+
+        //// GOAL 3-3) A loop where the comparison that’s used to determine whether to continue iterating the loop is a “<=” operator
+
+        //int totalValue = 0; // Initalise an integer called totalValue with a value of zero
+        //do 
+        //{
+        //    Console.Write("\nPlease enter a numerical value, this will be added to the current total: "); // Output this text to the console
+        //    totalValue = totalValue + Convert.ToInt32(Console.ReadLine()); // Take the value the user inputs and add it the current value of totalValue
+        //    Console.WriteLine("\nThe current total is: " + totalValue); // Concatenate the current value of totalValue to this string
+        //} // End DO WHILE
+        //while (totalValue <= 12500); // After the loop has run once we check if totalValue is less or equal to 12500 if it is we iterate through the loop again
+
+        //Console.WriteLine("\nThe total has exceeded 12500 and the application will now exit. Thank You!.\n"); // Output this text to the console
+
+        /* ---------------------------------------------------------------- PART 4 ----------------------------------------------------------------------------------- */
+
+        Console.WriteLine("Console App Assignment - Part 4"); // Output this text to the console then start a new line
         Console.WriteLine("-------------------------------\n");
 
-        // GOAL 3-1) A loop where the comparison that’s used to determine whether to continue iterating the loop is a “<” operator
-        for(int c = 1; c < 11; c++) // This FOR loop will count from 1 to 10 using the integer c as a counter
+        // GOAL 4-1) A list of strings where each item in the list is unique
+
+        List<string> c64Games = new List<string>() { "Summer Games", "Jumpman", "Movie Monster Game", "Subterranea", "Alleykat", "Winter Games", "Lee", "Marauder", "Battle Valley", "Street Sports Baseball", "Impossible Mission", "Gateway to Apshai", "Stormlord", "Idridis Alpha" }; // Instanstiate a new instance of the list object called c64Games and assign these fourteen values
+
+        // GOAL 4-2) Ask the user to input text to search for in the list
+
+        Console.WriteLine("Commodore 64 Game Database"); // Output this text to the console
+        Console.WriteLine("--------------------------"); // Output this text to the console
+
+        Console.Write("\nPlease enter a game name to search for: "); // Output this text to the console
+        string searchString = Console.ReadLine(); // Take the user input and assign it to the string searchString
+        Console.WriteLine(); // Add a blank line to the console
+
+        // 4-3) A loop that iterates through the list and then displays the index of the list that contains matching text on the screen
+
+        bool matchFound = false; // A boolean variable to track if we have found a match within our database which we initally set to false
+
+        for (int g = 0; g <= 13; g++) // Start a FOR loop using integer variable g as a counter start at zero and going up to thirteen
         {
-            Console.WriteLine("The loop has run " + c + " times."); // Concatenate c into this string and out it to the console.
+
+            if (c64Games[g].ToUpper().Contains(searchString.ToUpper())) // We check if the current value of c64Games contains the value in searchString, we also convert both values to uppercase to add with case accuracy, then if we find a match
+            {
+                // The current value of the counter g will represet our index position in our list
+                Console.WriteLine("Game found at index position " + g); // We concatenate that value to this string then output it to the console
+                matchFound = true; // We can set this to true as we have a match
+
+                // GOAL 4-5) Add code to the loop that stops it from executing once a match has been found
+                break; // We break the loop here as we have found a match and there is no need to continue
+            } // End IF
+
         } // End FOR
 
-        // GOAL 3-3) A loop where the comparison that’s used to determine whether to continue iterating the loop is a “<=” operator
+        // GOAL 4-4) Add code to the loop that tells a user if they put in text that isn’t in the list
 
-        int totalValue = 0; // Initalise an integer called totalValue with a value of zero
-        do 
+        if (!matchFound) // If after the above FOR loop has completed matchFound is still false then we had no match and we inform the user
         {
-            Console.Write("\nPlease enter a numerical value, this will be added to the current total: "); // Output this text to the console
-            totalValue = totalValue + Convert.ToInt32(Console.ReadLine()); // Take the value the user inputs and add it the current value of totalValue
-            Console.WriteLine("\nThe current total is: " + totalValue); // Concatenate the current value of totalValue to this string
-        } // End DO WHILE
-        while (totalValue <= 12500); // After the loop has run once we check if totalValue is less or equal to 12500 if it is we iterate through the loop again
+            Console.WriteLine("No match found in the database, sorry!."); // Output this text to the console
+        } // End IF
 
-        Console.WriteLine("\nThe total has exceeded 12500 and the application will now exit. Thank You!."); // Output this text to the console
+        //foreach(string c64Game in c64Games) // Iterate through each element within the c64Games list assigning the current value to c64Game
+        //{
+        //    Console.WriteLine(c64Game);
+        //} // End FOR EACH
 
         Console.ReadLine(); // Pause the application
 
