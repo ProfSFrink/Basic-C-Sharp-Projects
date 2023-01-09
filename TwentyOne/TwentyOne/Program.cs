49eb6d6422443dc1177b0d08b0cdc3db62e11697
Steven Partlow <97795561+ProfSFrink@users.noreply.github.com>
2023-01-09 11:59:54 +0000

    UPDATE Polymorphism Tutorial

--- TwentyOne/TwentyOne/Program.cs
+++ TwentyOne/TwentyOne/Program.cs
@@ -18,15 +18,16 @@ namespace TwentyOne
     {
         static void Main(string[] args)
         {
+            /* INHERITANCE TUTORIAL - Old code left over from the inheritance tutorial */
             //TwentyOneGame game = new TwentyOneGame(); // Instantiate a new instance of the TwentyOneGame class called TwentyOneGame
             //game.Players = new List<string>() { "Jesse", "Bill", "Joe" }; // Add three values to the Players property of the game object as a list of strings
             //game.ListPlayers();
             //game.Play();
 
-            //Console.ReadLine();
-
-            Deck deck = new Deck(); // Instanstiate a new instance of the Deck object called deck
-            deck.Shuffle(3); // Execute the Shuffle method attached to the instance of our Deck class called deck, shuffle the deck three times
+            /* POLOYMORPHISM TUTORIAL - Example of polymorphism */
+            //Game game = new TwentyOneGame();
+            //Deck deck = new Deck(); // Instanstiate a new instance of the Deck object called deck
+            //deck.Shuffle(3); // Execute the Shuffle method attached to the instance of our Deck class called deck, shuffle the deck three times
 
             foreach (Card card in deck.Cards) // For each card object in the object Deck property Cards
             {
