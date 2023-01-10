e454cf35e825fd424336cf1c8ed2c6cc0465cbe9
Steven Partlow <97795561+ProfSFrink@users.noreply.github.com>
2023-01-10 15:36:03 +0000

    UPDATE Abstract Classes Assignment:
    - UPDATE Remove unecessary using statements

--- Abstract_Classes_Assign/Abstract_Classes_Assign/Program.cs
+++ Abstract_Classes_Assign/Abstract_Classes_Assign/Program.cs
@@ -15,10 +15,6 @@
 // 5) Inside the Main() method, instantiate an Employee object with firstName "Sample" and lastName "Student". Call the SayName() method on the object. - DONE
 
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Abstract_Classes_Assign
 {
