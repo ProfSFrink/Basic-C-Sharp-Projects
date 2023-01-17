013e95b241304ae9d7a8d48f57174d97179f8f40
Steven Partlow <97795561+ProfSFrink@users.noreply.github.com>
2023-01-17 13:21:21 +0000

    UPDATE Lambda Expressions Assignment

--- Lambda_Express_Assign/Lambda_Express_Assign/Program.cs
+++ Lambda_Express_Assign/Lambda_Express_Assign/Program.cs
@@ -11,13 +11,11 @@
 // 1) In the Main() method, create a list of at least 10 employees. Each employee should have a first and last name, as well as an Id. At least two employees should have the first name "Joe". - DONE
 // 2) Using a foreach loop, create a new list of all employees with the first name "Joe". - DONE
 // 3) Perform the same action again, but this time with a lambda expression. - DONE
-// 4) Using a lambda expression, make a list of all employees with an Id number greater than 5.
+// 4) Using a lambda expression, make a list of all employees with an Id number greater than 5. - DONE
 
 using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Lambda_Express_Assign
 {
