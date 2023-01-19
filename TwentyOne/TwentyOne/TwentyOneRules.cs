f610090f5af7de6e6537796dbc8884cb64417054
Steven Partlow <97795561+ProfSFrink@users.noreply.github.com>
2023-01-19 11:36:17 +0000

    UPDATE Putting It All Together Part 6:
    - ADD For loop to iterate through the result array and calculate hand values for each ace in the hand then return the result array
    - UPDATE GetAllPossibleHandValues method change public to private as this will only be used from within this class
    - ADD PossibleValue integer array to CheckForBlackJack and execute GetAllPossibleValues and store the returned result with the new array
    - ADD Check the maximum value of possibleValues
    - ADD A logic check if the maximum value of possibleValues is equal to 21 then we have a blackjack else we return false

--- TwentyOne/TwentyOne/TwentyOneRules.cs
+++ TwentyOne/TwentyOne/TwentyOneRules.cs
@@ -46,19 +46,31 @@ namespace TwentyOne
         /* TwentyOneRules METHODS */
 
         /* A method were we pass in a Hand of cards and then calculate all the possible values that hand could have and return it as an integer array */
-        public static int[] GetAllPossibleHandValues(List<Card> Hand) 
+        private static int[] GetAllPossibleHandValues(List<Card> Hand) 
         {
             int aceCount = Hand.Count(x => x.Face == Face.Ace); // Use a lambda expression to count the number of cards in the passed in Hand that have a face value equal to ace
             int[] result = new int[aceCount + 1]; // Create an array callled result the size of which will be the value of aceCount plus one
-            int value = Hand.Sum(x => _cardValues[x.Face]); // Use a lambda expression to get the sum value of all cards in the hand
+            int value = Hand.Sum(x => _cardValues[x.Face]); // Use a lambda expression to calculate the intial value of our hand not accounting for aces
             result[0] = value; // Take the variable value and assign it to the first element of the array result
-            if (result.Length == 1) return result; // If we only have one possible value of our hand then we can return the result array and quit the method at this point
+
+            if (result.Length == 1) return result; // If the result array only has one element this means that our hand has no aces which means it cannot have anymore possible values so we can end the method and return the result array at this point
+
+            for(int i = 1; i < result.Length; i++) // Iterate through the result array we start at the second element as we have already calulated the intial hand value and assigned it the result array
+            {
+                value += (i * 10); // Take the hands base value then multiply i by 10, each iteration represents one ace in our hand so we on the first loop we calulate the hand value plus one ace, then on the second loop it would the hand value plus two aces
+                result[i] = value; // Take the newly calculated value and assign it to the result array
+            } // End FOR
+
+            return result; // We have now calulated all possible values for the passed in hand of cards so we can return the result array
 
         } // End GetAllPossibleHandValues
 
         public static bool CheckForBlackJack(List<Card> Hand)
         {
-
+            int[] possibleValues = GetAllPossibleHandValues(Hand); // Execute our GetAllPossibleValues method passing in our hand and storing the returned array to possibleValues
+            int value = possibleValues.Max(); // Get the maximum possible value from the possibleValues array and assign it to the integer value
+            if (value == 21) return true; // The hand has a blackjack so we return true
+            else return false; // Any other value cannot be a blackjack so we retrun false
         } // End CheckForBlackJack Method
 
     } // End TwentyOneRules Definition
