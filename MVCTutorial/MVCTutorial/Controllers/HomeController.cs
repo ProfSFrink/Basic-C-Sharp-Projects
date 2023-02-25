24175c5a86103c97e4209fced933f3f98cf5a52d
Steven Partlow <profsfrink@gmail.com>
2023-02-25 12:09:41 +0000

    COMPLETE MVC Tutorial Part 3 & 3
    User.cs
    - ADD Integer Id property
    - ADD String FirstName property
    - ADD String LastName property
    - ADD Integer Age property
    
    HomeController
    - ADD Create instance of user
    - ADD Set value of user properties
    - ADD Pass user in to Index view
    
    Index.cshtml
    - ADD Display each property of user model instance
    
    Readme.md
    - ADD Tutorial description

--- MVCTutorial/MVCTutorial/Controllers/HomeController.cs
+++ MVCTutorial/MVCTutorial/Controllers/HomeController.cs
@@ -1,30 +1,53 @@
-﻿using System;
+﻿// Advanced Software and Web Developer Diploma
+// Part X: C# and .NET Framework - Part 2
+// by Pitman Training / The Tech Academy
+
+// MODULE 11: Creating MVC Applications
+// AUTHOR: Steven Partlow
+// DATE: 25/02/2023
+
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using MVCTutorial.Models; // Use any Model we have defined
 
 namespace MVCTutorial.Controllers
 {
+
     public class HomeController : Controller
     {
-        public ActionResult Index()
-        {
-            return View();
-        }
 
-        public ActionResult About()
+        public ActionResult Index() // A method to call the Index view
+        {
+            User user = new User(); // Create a new instance of the user Model
+
+            // Set the properties of the user Model
+            user.Id = 1;
+            user.FirstName = "Steven";
+            user.LastName = "Partlow";
+            user.Age = 43;
+
+            return View(user); // Return the Index view passing in the user model object
+        } // End INDEX
+
+        public ActionResult About() // A method to call the About view
         {
             ViewBag.Message = "Your application description page.";
 
-            return View();
-        }
+            throw new Exception("Invalid page");
 
-        public ActionResult Contact()
+            return View(); // Return the About view passing in the user model object
+        } // End ABOUT
+
+        public ActionResult Contact(int id = 0) // A method to call the Contact view
         {
-            ViewBag.Message = "Your contact page.";
+            ViewBag.Message = id;
 
-            return View();
-        }
-    }
-}
+            return View(); // Return the Contact view passing in the user model object
+        } // End CONTACT
+
+    } // End CLASS HomeController
+
+} // End MVCTutorial.Controllers NAMESPACE
